78c806846a5dde48b0807802ef5056e95d4dc1d6
Alice Heaton <a.heaton@nhm.ac.uk>
2014-11-11 17:07:35 +0000

    Continous integration #9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name='ckanpackager',
-  version='0.1',
+  version='0.2',
   description='Service to package CKAN data into ZIP files and email the link to the file to users',
   url='http://github.com/NaturalHistoryMuseum/ckanpackager',
   packages=find_packages()
