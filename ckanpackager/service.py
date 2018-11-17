839dc7d495582f8d304910a454e0a1ffb76361c5
Josh Humphries <j.humphries@nhm.ac.uk>
2018-11-17 22:28:41 +0000

    Reformat the service module

--- ckanpackager/service.py
+++ ckanpackager/service.py
@@ -1,37 +1,41 @@
-"""Ckanpackager service
+import os
+from flask import Flask
+
+from ckanpackager.controllers.actions import actions
+from ckanpackager.controllers.error_handlers import error_handlers
+from ckanpackager.controllers.packagers import packagers
+from ckanpackager.controllers.status import status
+
+"""
+Ckanpackager service
 
 Run a web service to access statistics, perform actions and queue tasks
 """
-import os
 
-from flask import Flask, g
-from ckanpackager.controllers.status import status
-from ckanpackager.controllers.actions import actions
-from ckanpackager.controllers.packagers import packagers
-from ckanpackager.controllers.error_handlers import error_handlers
-
-# Create the application
+# create the application
 app = Flask(__name__)
 
-# Read configuration
+# read configuration
 app.config.from_object('ckanpackager.config_defaults')
 app.config.from_envvar('CKANPACKAGER_CONFIG')
 
-# Create folders if required
+# create folders if required
 if not os.path.exists(app.config['TEMP_DIRECTORY']):
     os.makedirs(app.config['TEMP_DIRECTORY'])
 if not os.path.exists(app.config['STORE_DIRECTORY']):
     os.makedirs(app.config['STORE_DIRECTORY'])
 
-# Register our blueprints
+# register our blueprints
 app.register_blueprint(status)
 app.register_blueprint(actions)
 app.register_blueprint(packagers)
 app.register_blueprint(error_handlers)
 
+
 def run():
-    """ Start the server """
-    global app
+    """
+    Start the server
+    """
     app.run(
         host=app.config['HOST'],
         port=int(app.config['PORT']),
@@ -41,6 +45,7 @@ def run():
         use_reloader=False
     )
 
-# Start server (debug mode)
+
+# start server (debug mode)
 if __name__ == '__main__':
     run()
